82b8696695c32051412f36b194be551b6fee8f9f
2Dokk <2dokyoung@gmail.com>
2022-05-15 23:10:12 +0900

    serviceSection

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -3,7 +3,8 @@ import React from "react";
 import clock from "../img/clock.svg";
 import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
-import teamwork from "../img/teamwork";
+import teamwork from "../img/teamwork.svg";
+import home2 from "../img/home2.png";
 
 const ServicesSection = () => {
   return (
@@ -15,33 +16,36 @@ const ServicesSection = () => {
         <div className="cards">
           <div className="card">
             <div className="icon">
-              <img src={clock} />
+              <img alt="icon" src={clock} />
               <h3>Efficient</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
           <div className="card">
             <div className="icon">
-              <img src={teamwork} />
+              <img alt="icon" src={teamwork} />
               <h3>Teamwork</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
           <div className="card">
             <div className="icon">
-              <img src={diaphragm} />
+              <img alt="icon" src={diaphragm} />
               <h3>Diaphragm</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
           <div className="card">
             <div className="icon">
-              <img src={money} />
+              <img alt="icon" src={money} />
               <h3>Affordable</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
         </div>
+      </div>
+      <div className="image">
+        <img alt="camera" src={home2} />
       </div>
     </div>
   );
