00a20c5941642b31efc290020cffa844201bf64e
2Dokk <2dokyoung@gmail.com>
2022-05-18 22:15:02 +0900

    20220518

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,11 @@
 import React from "react";
 import home1 from "../img/home1.png";
+//Styled
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
+    <About>
       <div className="description">
         <div className="title">
           <div className="hide">
@@ -26,8 +28,18 @@ const AboutSection = () => {
       <div className="image">
         <img src={home1} alt="guy with a camera" />
       </div>
-    </div>
+    </About>
   );
 };
+
+//Styled components
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10;
+  color: white;
+`;
 
 export default AboutSection;
